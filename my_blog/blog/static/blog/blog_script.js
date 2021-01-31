840dff43620880f2d2905faeee0a02ecfca44a67
Alireza <alireza.niary@gmail.com>
2021-01-31 15:10:18 +0330

    index.html views and urls added

---
+++ my_blog/blog/static/blog/blog_script.js
@@ -0,0 +1,11 @@
+$(window).scroll(function () {
+  var s = $(window).scrollTop(),
+        d = $(document).height(),
+        c = $(window).height();
+        scrollPercent = (s / (d-c)) * 100;
+        var position = scrollPercent;
+
+   $("#progressbar").attr('style', 'width: ' + position + '%; transition: none;');
+
+});
+
