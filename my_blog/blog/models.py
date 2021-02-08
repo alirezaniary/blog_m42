bba8750dedc427f409f1f26f57a349c8a8f0edec
Alireza <alireza.niary@gmail.com>
2021-02-08 09:31:06 +0330

    apllied changes to model and migrate

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 class Article(models.Model):
     is_active = models.BooleanField('نمایش عمومی؟', default=True)
     is_valid = models.BooleanField('تایید شده؟', default=False)
-    pub_date = models.DateField('تاریخ انتشار', default=timezone.now)
+    pub_date = models.DateField('تاریخ انتشار', auto_now_add=True)
     val_date = models.DateField('تاریخ تایید', null=True, blank=True)
-    img_path = models.FileField('محل ذخیره تصویر', upload_to='%Y/%m/%d/')
+    img_path = models.ImageField('محل ذخیره تصویر', upload_to='%Y/%m/%d/')
     title = models.CharField('عنوان مقاله', max_length=150)
     text = models.TextField('متن مقاله', )
     author = models.ForeignKey(
@@ -18,8 +18,8 @@ class Article(models.Model):
     tag = models.ManyToManyField('Tag', blank=True, verbose_name='برچسب')
     topic = models.ForeignKey(
         'Topic', on_delete=models.PROTECT, verbose_name='دسته بندی')
-    like = models.ManyToManyField(
-        'BlogUser', through='ArticleLike', related_name='Alikes', verbose_name='پسند')
+    user_liked = models.ManyToManyField(
+        'BlogUser', through='ArticleLike', related_name='alikes', verbose_name='پسند')
 
     def __str__(self):
         return self.title
@@ -28,7 +28,7 @@ class Article(models.Model):
 class Comment(models.Model):
     text = models.TextField('متن نظر', )
     is_valid = models.BooleanField('تایید شده؟', default=False)
-    pub_date = models.DateField('تاریخ انتشار', default=timezone.now)
+    pub_date = models.DateField('تاریخ انتشار', auto_now_add=True)
     val_date = models.DateField('تاریخ تایید', null=True, blank=True)
     user = models.ForeignKey('BlogUser', on_delete=models.CASCADE,
                              related_name='comments', verbose_name='نویسنده')
@@ -38,8 +38,8 @@ class Comment(models.Model):
         'Article', on_delete=models.CASCADE, verbose_name='مقاله')
     response_to = models.ForeignKey('self', on_delete=models.CASCADE, null=True,
                                     blank=True, related_name='response', verbose_name='پاسخ به')
-    like = models.ManyToManyField(
-        'BlogUser', through='CommentLike', related_name='Clikes', verbose_name='پسند')
+    uesr_liked = models.ManyToManyField(
+        'BlogUser', through='CommentLike', related_name='clikes', verbose_name='پسند')
 
     def __str__(self):
         return self.text
@@ -51,14 +51,14 @@ class BlogUser(User):
         return f'user/user_{self.id}/{filename}'
 
     phone_number = models.CharField('شماره تلفن', max_length=11)
-    img_path = models.FileField('محل ذخیره عکس', upload_to=user_directory_path)
+    img_path = models.ImageField('محل ذخیره عکس', upload_to=user_directory_path)
     is_author = models.BooleanField('نویسنده است؟', default=False)
     is_editor = models.BooleanField('ویراستار است؟', default=False)
     is_manager = models.BooleanField('مدیر است؟', default=False)
     is_inactive = models.BooleanField('غیر فعال شده؟', default=False)
-    follow = models.ManyToManyField(
+    followers = models.ManyToManyField(
         'self', symmetrical=False, through='Follow', related_name='followedBy', verbose_name='دنبال کردن')
-    bookmark = models.ManyToManyField(
+    bookmarked_articles = models.ManyToManyField(
         'Article', through='Bookmark', related_name='bookmarkedBy', verbose_name='نشان کردن')
     bio = models.CharField('درباره من', max_length=250)
 
@@ -121,9 +121,9 @@ class CommentLike(models.Model):
 
 class Follow(models.Model):
     author = models.ForeignKey(
-        'BlogUser', on_delete=models.CASCADE, related_name='followers', verbose_name='نویسنده')
+        'BlogUser', on_delete=models.CASCADE, related_name='follower', verbose_name='نویسنده')
     user = models.ForeignKey('BlogUser', on_delete=models.CASCADE,
-                             related_name='followings', verbose_name='کاربر')
+                             related_name='following', verbose_name='کاربر')
 
     class Meta:
         constraints = [
